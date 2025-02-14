6cfd24e42b01d23dfa1bd35d4ee23db6f834dd26
Upendra Kumar <uk06704@gmail.com>
2025-02-14 18:26:21 +0530

    added configuration for eureka client and resilience4j, updated application properties, and initialized MySQL database

--- MicroService/order_service/src/main/java/com/upendra/order_service/config/GeneralConfiguration.java
+++ MicroService/order_service/src/main/java/com/upendra/order_service/config/GeneralConfiguration.java
@@ -3,11 +3,13 @@ package com.upendra.order_service.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
-public class OpenAPIConfig {
+public class GeneralConfiguration {
 
     @Bean
     public OpenAPI customOpenAPI(@Value("${spring.application.name}") String appName,
@@ -18,5 +20,11 @@ public class OpenAPIConfig {
                         .title(appName)
                         .version(appVersion)
                         .description(appDescription));
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 }
--- MicroService/order_service/src/main/java/com/upendra/order_service/config/RestClientBeans.java
+++ MicroService/order_service/src/main/java/com/upendra/order_service/config/RestClientBeans.java
@@ -7,13 +7,21 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+import java.net.URI;
+
 @Configuration
 public class RestClientBeans {
 
+    private final RestClient.Builder restClientBuilder;
+
+    public RestClientBeans(RestClient.Builder restClientBuilder) {
+        this.restClientBuilder = restClientBuilder;
+    }
+
     @Bean
     public InventoryServiceClient inventoryServiceClient() {
-        RestClient restClient = RestClient.builder()
-                .baseUrl("http://localhost:8081")
+        RestClient restClient = restClientBuilder
+                .baseUrl(URI.create("http://inventory-service"))
                 .build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
